9b6052eb37208ce3bd2dc063f5a149d917da5651
Dom Charlesworth <dgc336@gmail.com>
2015-06-12 07:25:53 +0100

    Added FPS Updating to the anim loop

--- js/verlet/main.js
+++ js/verlet/main.js
@@ -1,8 +1,8 @@
 var canvas,
-		ctx,
-		width,
-		height,
-		cloth;
+		ctx, 
+		cloth,
+		lastCallTime,
+		fps;
 
 window.requestAnimFrame = (function() {
 
@@ -17,9 +17,9 @@ window.requestAnimFrame = (function() {
 
 canvas = document.getElementById( 'canvas' );
 ctx = canvas.getContext( '2d' );
+ctx.canvas.width = window.innerWidth, ctx.canvas.height = window.innerHeight;
 
-width = ctx.canvas.width = window.innerWidth, height = ctx.canvas.height = window.innerHeight;
-cloth = new Cloth( 61, 10, { x: 10, y: 30 }, ctx );
+cloth = new Cloth();
 
 var mouse = { minPoint: null };
 
@@ -34,11 +34,11 @@ canvas.onmouseup = function (e) {
 
 canvas.onmousemove = function (evt) {
 
-	if ( mouse.minPoint ) 
+	if ( mouse.minPoint )
 		mouse.minPoint.setCurrent( new Vector(evt.offsetX, evt.offsetY) );
-	
+
   evt.preventDefault();
-	
+
 };
 
 function toggleColourful() { Cloth.prototype.RENDER = !Cloth.prototype.RENDER; }
@@ -46,15 +46,54 @@ function toggleConstraints() { Cloth.prototype.DRAW_CONSTRAINTS = !Cloth.prototy
 function togglePoints() { Cloth.prototype.DRAW_POINTS = !Cloth.prototype.DRAW_POINTS; }
 function toggleRainbow() { Cloth.prototype.RAINBOW = !Cloth.prototype.RAINBOW; }
 
+var s1 = new Slider('#width',{ min:1, max:81, step:4});
+s1.on('change', function (v) {
+	Cloth.prototype.WIDTH = v.newValue;
+	cloth = new Cloth();
+});
+
+var s2 = new Slider('#height',{ min:1, max:80, step:1});
+s2.on('change', function (v) {
+	Cloth.prototype.HEIGHT = v.newValue;
+	cloth = new Cloth();
+});
+
+
+var s3 = new Slider('#hspacing',{ min:1, max:60, step:1});
+s3.on('change', function (v) {
+	Cloth.prototype.SPACING.x = v.newValue;
+	cloth = new Cloth();
+});
+
+var s4 = new Slider('#vspacing',{ min:1, max:60, step:1});
+s4.on('change', function (v) {
+	Cloth.prototype.SPACING.y = v.newValue;
+	cloth = new Cloth();
+});
+
+function setFPSCounter( fps ) {
+
+	var fpsCounter = document.getElementById( 'fps-counter' );
+	fpsCounter.textContent = fps;
+	
+}
+
+
 (function animLoop() {
 
+	if (!lastCallTime) 
+		lastCallTime = Date.now(), fps = 0;
+
+	
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.lineWidth = 1;
 
 	cloth.update();
-
+	
+	fps = 1000/(new Date().getTime() - lastCallTime);
+	lastCallTime = Date.now();
+	
+	setFPSCounter(fps.toPrecision( 4 ));
 	requestAnimFrame(animLoop);
 
 })();
-
-
